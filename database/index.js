2e4cc566dab91cae80eaa83970c13d637d39969f
SeungBin Kim <seungbin0508@gmail.com>
2021-10-14 21:42:04 +0900

    Create SNLE database and User TABLE
    By using IF NOT EXISTS, create database and/or table

--- database/index.js
+++ database/index.js
@@ -4,7 +4,38 @@ const pool = mysql.createPool({
 	host: 'localhost',
 	user: 'test',
 	password: 'test',
-	database: 'SNLE'
 })
 
-connection.connect()
+const connection = await pool.getConnection()
+
+try {
+	await connection.beginTransaction()
+	const createDatabase = `
+	CREATE DATABASE IF NOT EXISTS SNLE;
+	`
+	const useDatabase = `
+	USE SNLE;
+	`
+	const query = `
+create table IF NOT EXISTS user
+(
+	name varchar(20) not null,
+	nickname varchar(20) null,
+	phone varchar(11) not null primary key unique,
+	enlist date not null,
+	withdrawal date null,
+	participation smallint default 100 null,
+	admin tinyint(1) default 0 null,
+	password varchar(20) null
+);`
+	await connection.query(createDatabase)
+	await connection.query(useDatabase)
+	await connection.query(query)
+	await connection.commit()
+	console.log('Database and user table are ready')
+} catch (err) {
+	await connection.rollback()
+	console.error(err)
+} finally {
+	connection.release()
+}
