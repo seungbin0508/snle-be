21397ca162925918ddac2ab9b7a8265cbb907300
SeungBin Kim <seungbin0508@gmail.com>
2021-10-15 10:26:14 +0900

    Add encryptWithSalt method

--- util/crypto.js
+++ util/crypto.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv'
+import crypto from 'crypto'
 
 dotenv.config()
 
 export default class Crypto {
-
+	static async encryptWithSalt(password) {
+		const salt = (await crypto.randomBytes(64)).toString('hex')
+		return crypto.pbkdf2Sync(password, salt, Number(process.env.ITERATION), 64, 'sha1').toString('hex')
+	}
 }
