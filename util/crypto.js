95af2c4e490e35a6703cfad52e4e23a982f6d258
SeungBin Kim <seungbin0508@gmail.com>
2021-10-15 12:29:08 +0900

    Update encryptWithSalt to return { encryptedPassword, salt }
    Formerly it returned without salt key

--- util/crypto.js
+++ util/crypto.js
@@ -6,6 +6,7 @@ dotenv.config()
 export default class Crypto {
 	static async encryptWithSalt(password) {
 		const salt = (await crypto.randomBytes(64)).toString('hex')
-		return crypto.pbkdf2Sync(password, salt, Number(process.env.ITERATION), 64, 'sha1').toString('hex')
+		const encryptedPassword = crypto.pbkdf2Sync(password, salt, Number(process.env.ITERATION), 64, 'sha1').toString('hex')
+		return { encryptedPassword, salt }
 	}
 }
