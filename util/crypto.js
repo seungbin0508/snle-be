b1c7a76d977e68ab0abb288b80d1458f1f409297
SeungBin Kim <seungbin0508@gmail.com>
2021-10-17 12:08:36 +0900

    Add JSDoc of encryptWithSalt method

--- util/crypto.js
+++ util/crypto.js
@@ -4,6 +4,11 @@ import crypto from 'crypto'
 dotenv.config()
 
 export default class Crypto {
+	/**
+	 *
+	 * @param password {string}
+	 * @returns {Promise<{encryptedPassword: string, salt: string}>}
+	 */
 	static async encryptWithSalt(password) {
 		const salt = (await crypto.randomBytes(64)).toString('hex')
 		const encryptedPassword = crypto.pbkdf2Sync(password, salt, Number(process.env.ITERATION), 64, 'sha1').toString('hex')
