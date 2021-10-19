abc1a516f832ef564b6ec42609f616e41b813877
SeungBin Kim <seungbin0508@gmail.com>
2021-10-19 21:44:26 +0900

    Add verifyAccessToken method
    The method returns success, phone and admin

--- util/session.js
+++ util/session.js
@@ -1,18 +1,33 @@
 import jwt from 'jsonwebtoken'
+
 export default class Session {
 	/**
 	 * @param user {Object<phone: string, admin: boolean>}
-	 * @returns {Promise<*>}
+	 * @returns {string}
 	 */
-	static async getAccessToken(user) {
-			const payload = {
-				phone: user.phone,
-				admin: user.admin
-			}
+	static getAccessToken (user) {
+		const payload = {
+			phone: user.phone,
+			admin: user.admin
+		}
 
-			return jwt.sign(payload, process.env.JWTSECRET, {
-				algorithm: 'HS256',
-				expiresIn: '1h'
-			})
+		return jwt.sign(payload, process.env.JWTSECRET, {
+			algorithm: 'HS256',
+			expiresIn: '1h'
+		})
+	}
+
+	/**
+	 * @param token {string}
+	 * @returns {{success: true, phone: string, admin: boolean}|{success: false}}
+	 */
+	static verifyAccessToken (token) {
+		try {
+			const { phone, admin } = jwt.verify(token, process.env.JWTSECRET)
+			return { success: true, phone, admin }
+		} catch (err) {
+			console.error(err)
+			return { success: false }
+		}
 	}
 }
