cae50dab8725aeb2df16c631ea32360025112d5f
SeungBin Kim <seungbin0508@gmail.com>
2021-11-02 09:45:55 +0900

    Refactor client to redisClient
    Previously, the name was too ambiguous

--- redis/index.js
+++ redis/index.js
@@ -4,12 +4,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const client = createClient({
+const redisClient = createClient({
 	url: `redis://admin:${process.env.REDIS_PASSWORD}@redis-19593.c290.ap-northeast-1-2.ec2.cloud.redislabs.com:19593`,
 })
 
-client.on('error', err => console.error('Redis Client Error', err))
+redisClient.on('error', err => console.error('Redis Client Error', err))
 
-await client.connect()
+await redisClient.connect()
 
-export default client
+export default redisClient
